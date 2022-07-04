4eb72b9bf703842fc3d3d1e36f6a03cffe8c5f02
Rosdell Ouma <abonyorosdell@gmail.com>
2022-07-04 20:52:46 +0300

    react understanding element and jsx

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 import React from "react"
 import ReactDOM from "react-dom"
-ReactDOM.render(<div>React Element</div> document.getElementById('root'));
-
+ReactDOM.render(<div>React Element</div>, document.getElementById("root"))
