7f441b833186329c4d6f94d72318989dc8ce16aa
Rosdell Ouma <abonyorosdell@gmail.com>
2022-07-05 02:06:23 +0300

    refactoring and making code cleaner

--- src/App.js
+++ src/App.js
@@ -1,35 +1,29 @@
 import React, { Component } from "react"
 class App extends Component {
-  constructor() {
-    super()
-    this.state = { displayBio: false }
-    this.readMore = this.readMore.bind(this)
-    this.showLess = this.showLess.bind(this)
+  state = { displayBio: false }
+
+  toggleDisplayBio = () => {
+    this.setState({ displayBio: !this.state.displayBio })
   }
-  readMore() {
-    this.setState({ displayBio: true })
-  }
-  showLess() {
-    this.setState({ displayBio: false })
-  }
-  render() {
+  render = () => {
     return (
       <div>
-        <h1>Hello!</h1>
-        <p>My name is Rosdell and I am a software Developer</p>
-        <p>I am always looking forward to working on a meaningful project</p>
+        <h1>Yeah Man!</h1>
+        <p>Call me Rosdell,& I am a software Developer</p>
+        <p>I am always looking forward to working on a meaningful project.</p>
         {this.state.displayBio ? (
           <div>
-            <p>I live in Kampala Uganda,and I code everyday</p>
+            <p>I am currently based in Kampala ,and I code everyday.</p>
             <p>
-              my favorite language is JavaScript and I think React.js is awesome
+              my favorite language is JavaScript and I think React.js made it
+              more fun.
             </p>
-            <p>Besides coding i also do Ziki and swimming</p>
-            <button onClick={this.showLess}>Show Less</button>
+            <p>Besides coding I also do Ziki and Basketball</p>
+            <button onClick={this.toggleDisplayBio}>Show Less</button>
           </div>
         ) : (
           <div>
-            <button onClick={this.readMore}>Read More</button>
+            <button onClick={this.toggleDisplayBio}>Read More</button>
           </div>
         )}
         {/* {bio} */}
