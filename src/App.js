1e80c0a2db3a01e922febefb65b0eeb2be9ea43f
Rosdell Ouma <abonyorosdell@gmail.com>
2022-07-04 23:14:02 +0300

    exploring classes and inheritance

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello!</h1>
+        <p>My name is Rosdell and I am a software Developer</p>
+        <p>I am always looking forward to working on a meaningful project</p>
+      </div>
+    )
+  }
+}
+export default App
