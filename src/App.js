68e2dfec9af6f2eadf1eb26354f1f06de041c612
Rosdell Ouma <abonyorosdell@gmail.com>
2022-07-05 01:27:00 +0300

    we have added buttons and made it to showless and showMore button to work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React, { Component } from "react"
 class App extends Component {
+  constructor() {
+    super()
+    this.state = { displayBio: false }
+    this.readMore = this.readMore.bind(this)
+    this.showLess = this.showLess.bind(this)
+  }
+  readMore() {
+    this.setState({ displayBio: true })
+  }
+  showLess() {
+    this.setState({ displayBio: false })
+  }
   render() {
     return (
       <div>
         <h1>Hello!</h1>
         <p>My name is Rosdell and I am a software Developer</p>
         <p>I am always looking forward to working on a meaningful project</p>
+        {this.state.displayBio ? (
+          <div>
+            <p>I live in Kampala Uganda,and I code everyday</p>
+            <p>
+              my favorite language is JavaScript and I think React.js is awesome
+            </p>
+            <p>Besides coding i also do Ziki and swimming</p>
+            <button onClick={this.showLess}>Show Less</button>
+          </div>
+        ) : (
+          <div>
+            <button onClick={this.readMore}>Read More</button>
+          </div>
+        )}
+        {/* {bio} */}
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -2,36 +2,3 @@ import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
 ReactDOM.render(<App />, document.getElementById("root"))
-
-class Animal {
-  constructor(name, age) {
-    this.name = name
-    this.age = age
-  }
-  speak() {
-    console.log("I am", this.name, "and i am", this.age, "years old")
-  }
-}
-// const Animal1 = new Animal("simba", 3)
-// Animal1.speak()
-
-class lion extends Animal {
-  constructor(name, age, furColor, speed) {
-    super(name, age)
-    this.furColor = furColor
-    this.speed = speed
-  }
-  roar() {
-    console.log(
-      "i have",
-      this.furColor,
-      "fur",
-      "and i can run at",
-      this.speed,
-      "metre/minute"
-    )
-  }
-}
-const lion1 = new lion("komba", 6, "brown", 25)
-lion1.speak()
-lion1.roar()
