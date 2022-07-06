16987f85132a4a8e18e0f66865543a91f74ae469
Rosdell Ouma <abonyorosdell@gmail.com>
2022-07-06 16:12:21 +0300

    le-continure

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react"
+import Projects from "./Projects"
 class App extends Component {
   state = { displayBio: false }
 
@@ -26,7 +27,8 @@ class App extends Component {
             <button onClick={this.toggleDisplayBio}>Read More</button>
           </div>
         )}
-        {/* {bio} */}
+        <hr />
+        <Projects />
       </div>
     )
   }
---
+++ src/data/projects.js
@@ -0,0 +1,29 @@
+import project1 from "../assets/project1.png"
+import project2 from "../assets/project2.png"
+import project3 from "../assets/project3.png"
+
+const PROJECTS = [
+  {
+    id: 1,
+    title: "Example React Application",
+    description:
+      "A React App that I built using Core Js and Core Web dev Concepts!",
+    link: "https://github.com/Rosdel/React-portfolio",
+    image: project1,
+  },
+  {
+    id: 2,
+    title: "My API",
+    description: "A Rest API that I built from scratch with node.Js",
+    link: "https://github.com/15Dkatz/spotify-api-wrapper",
+    image: project2,
+  },
+  {
+    id: 3,
+    title: "LET'S PLAY PIGGame",
+    description: "complete PigGame with js",
+    link: "https://github.com/Rosdel/PigGame",
+    image: project3,
+  },
+]
+export default PROJECTS
